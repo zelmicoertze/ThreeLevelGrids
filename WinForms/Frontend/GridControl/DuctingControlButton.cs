2891e239ae8fb5b2113f1fdb0a15fc3172933189
perezLamed <perez.lamed.vanniekerk@gmail.com>
2017-05-11 12:47:26 +0200

    building with Lamedal 1.1.6

--- WinForms/Frontend/GridControl/DuctingControlButton.cs
+++ WinForms/Frontend/GridControl/DuctingControlButton.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using DevExpress.Utils.Drawing.Helpers;
 
 namespace DuctingGrids.Frontend.GridControl
 {
